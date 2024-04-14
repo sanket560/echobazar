67a64cc9779161de593351e8daad3eace12258ae
sanket mane <sanketmmane@gmail.com>
2024-04-14 20:20:11 +0530

    implimented navbar

---
+++ src/utils/index.js
@@ -0,0 +1,27 @@
+export const navOptions = [
+    {
+      id: "home",
+      label: "Home",
+      path: "/",
+    },
+    {
+      id: "listing",
+      label: "All Products",
+      path: "/product/listing/all-products",
+    },
+    {
+      id: "listingMen",
+      label: "Men",
+      path: "/product/listing/men",
+    },
+    {
+      id: "listingWomen",
+      label: "Women",
+      path: "/product/listing/women",
+    },
+    {
+      id: "listingKids",
+      label: "kids",
+      path: "/product/listing/kids",
+    },
+  ];
