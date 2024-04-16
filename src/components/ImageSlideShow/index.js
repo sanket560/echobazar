a532b4beee5a42723a680112f7c9e1ea5978c9a3
sanket mane <sanketmmane@gmail.com>
2024-04-16 11:30:55 +0530

    Image slide show

---
+++ src/components/ImageSlideShow/index.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React, { useState } from 'react';
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
+const ImageSlideShow = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className="relative">
+      <Image
+        className="w-full h-96 transition-all duration-600"
+        src={images[currentIndex]}
+        alt={`Slide ${currentIndex}`}
+        width={1200}
+        height={800}
+      />
+      <button
+        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white h-32 text-black px-3 py-1 rounded-md transition-opacity duration-300 hover:shadow-md"
+        onClick={goToPrevious}
+      >
+        <IoIosArrowBack />
+      </button>
+      <button
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white h-32 text-black px-3 py-1 rounded-md transition-opacity duration-300 hover:shadow-md"
+        onClick={goToNext}
+      >
+       <IoIosArrowForward />
+      </button>
+    </div>
+  );
+};
+
+export default ImageSlideShow
