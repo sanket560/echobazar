5c54cdb6cb363777e020550fd5428d5d79765292
sanket560 <sanketmmane@gmail.com>
2024-04-18 12:53:38 +0530

    admin page

---
+++ src/app/admin/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='bg-gray-100 flex items-center justify-center h-screen px-5"'>Admin</div>
+  )
+}
+
+export default Admin
