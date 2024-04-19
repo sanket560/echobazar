e7cd527489e7138bf8d8cca087afe3df07262447
sanket560 <sanketmmane@gmail.com>
2024-04-19 17:25:31 +0530

    firebase configuration for Image storing

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAa2OaEe76paYZBPskuDl6-YJ1043jA7Uo",
+    authDomain: "echobazar-1c232.firebaseapp.com",
+    projectId: "echobazar-1c232",
+    storageBucket: "echobazar-1c232.appspot.com",
+    messagingSenderId: "872991012360",
+    appId: "1:872991012360:web:21889a98c2a88a98e0c3bd",
+    measurementId: "G-RX8MM62S11"
+};
+
+export const firebaseStorageURL = 'gs://echobazar-1c232.appspot.com'
