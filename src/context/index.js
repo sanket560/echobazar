46ab0f70065cbf05da630a93275229a8893291cd
sanket560 <sanketmmane@gmail.com>
2024-04-26 16:16:03 +0530

    added function to fetch all products

--- src/context/index.js
+++ src/context/index.js
@@ -1,12 +1,37 @@
 "use client";
 import Cookies from "js-cookie";
 import { createContext, useEffect, useState } from "react";
+const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
 
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(null);
   const [userInfo, setUserInfo] = useState(null);
+  const [allProduct , setAllProduct] = useState([]);
+  const [loading , setLoading ] = useState(false);
+
+  const fetchAllProduct = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/all-product`,
+        {
+          method: "GET",
+        }
+      );
+      const responseData = await response.json();
+      setAllProduct(responseData?.data);
+      setLoading(false)
+    } catch (error) {
+      console.error("Error fetching of all product:", error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchAllProduct();
+  },[])
+
   useEffect(() => {
     if (Cookies.get("token") !== undefined) {
       setIsLoggedIn(true);
@@ -19,7 +44,7 @@ export default function GlobalState({ children }) {
 
   return (
     <GlobalContext.Provider
-      value={{ isLoggedIn, setIsLoggedIn, userInfo, setUserInfo }}
+      value={{ isLoggedIn, setIsLoggedIn,loading, userInfo, setUserInfo,allProduct }}
     >
       {children}
     </GlobalContext.Provider>
