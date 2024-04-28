ff74964329a75a4d04db590547f6881fd21438f8
sanket560 <sanketmmane@gmail.com>
2024-04-28 13:18:58 +0530

    adding product functionality

--- src/app/admin/add-product/page.js
+++ src/app/admin/add-product/page.js
@@ -1,8 +1,5 @@
 "use client";
-import { MensForm } from "@/components/MensForm";
 import { PhoneForm } from "@/components/PhoneForm";
-import { SmartTVForm } from "@/components/SmartTVForm";
-import { WomensForm } from "@/components/WomensForm";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
@@ -10,7 +7,6 @@ import toast from "react-hot-toast";
 
 
 const AddNewProducut = () => {
-  const [selectedProductType, setSelectedProductType] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
@@ -46,39 +42,7 @@ const AddNewProducut = () => {
         <h2 className="text-xl font-semibold text-center mb-4">
           Add New Product
         </h2>
-        <div className="mb-4 md:w-96 mx-auto">
-          <label
-            htmlFor="productType"
-            className="block text-sm font-medium text-gray-700 mb-2"
-          >
-            Select Product Type
-          </label>
-          <select
-            id="productType"
-            value={selectedProductType}
-            onChange={(e) => setSelectedProductType(e.target.value)}
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-            required
-          >
-            <option value="">Select Product Type</option>
-            <option value="phone">Phone</option>
-            <option value="mens">Men's</option>
-            <option value="womens">Women's</option>
-            <option value="smart_tv">Smart TV</option>
-          </select>
-        </div>
-        {selectedProductType === "phone" && (
-          <PhoneForm onSubmit={handleFormSubmit} isLoading={isLoading}/>
-        )}
-        {selectedProductType === "mens" && (
-          <MensForm onSubmit={handleFormSubmit} isLoading={isLoading} />
-        )}
-        {selectedProductType === "womens" && (
-          <WomensForm onSubmit={handleFormSubmit} isLoading={isLoading} />
-        )}
-        {selectedProductType === "smart_tv" && (
-          <SmartTVForm onSubmit={handleFormSubmit} isLoading={isLoading} />
-        )}
+        <PhoneForm onSubmit={handleFormSubmit} isLoading={isLoading}/>
       </div>
     </div>
   );
