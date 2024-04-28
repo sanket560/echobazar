20a2d1e5d663dba0728eb36ffa89a26663cdb330
sanket560 <sanketmmane@gmail.com>
2024-04-28 13:15:18 +0530

    changed ImageSideShow Image

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,18 @@
 "use client";
 import FeatureCards from "@/components/FeatureCards";
 import ImageSlideShow from "./../components/ImageSlideShow/index";
+import AllProoductListing from "@/components/AllProductsListing";
+import img1 from "../Images/img1.jpg";
+import img2 from "../Images/img2.jpg";
+import img3 from "../Images/img3.jpg";
 
 export default function Home() {
-  const images = [
-    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  ];
+  const images = [img1, img2, img3];
   return (
-    <div className='bg-gray-100 container mx-auto'>
+    <div className="bg-gray-100 container mt-[43px] md:mt-0 mx-auto">
       <ImageSlideShow images={images} />
       <FeatureCards />
+      <AllProoductListing />
     </div>
   );
 }
