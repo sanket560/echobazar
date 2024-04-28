b7241be45c27bc1f1d423aa8f2868b8c87d0cf23
sanket560 <sanketmmane@gmail.com>
2024-04-28 20:56:04 +0530

    fixed product detail page

--- src/app/product/[details]/page.js
+++ src/app/product/[details]/page.js
@@ -3,9 +3,5 @@ import ProductDetails from "./../../../components/ProductDetails/index";
 
 export default async function Product_Details({ params }) {
   const productDetailsData = await productById(params.details);
-  return (
-    <div className="">
-      <ProductDetails product={productDetailsData.data} />
-    </div>
-  );
+  return <ProductDetails product={productDetailsData.data} />;
 }
