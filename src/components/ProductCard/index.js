bb6d92ecfd2bfbd4d492f67f2d80be2d833d471c
sanket560 <sanketmmane@gmail.com>
2024-04-28 13:26:15 +0530

    added delete product API

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,7 +1,10 @@
 "use client";
+import { GlobalContext } from "@/context";
+import Cookies from "js-cookie";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useContext } from "react";
+import toast from "react-hot-toast";
 
 const imageStyles = {
   width: "400px",
@@ -10,28 +13,53 @@ const imageStyles = {
   padding: "8px",
 };
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product ,products, setProducts }) => {
+  const { setSelectedProductToUpdate } = useContext(GlobalContext);
   const pathname = usePathname();
+  const router = useRouter();
   const isAdminView = pathname.includes("admin");
+
+  const handleUpdateClick = () => {
+    setSelectedProductToUpdate(product);
+    router.push(`/admin/update-product`);
+  };
+
+  const deleteAProduct = async (productId) => {
+    try {
+      const res = await fetch(`/api/admin/delete-product?id=${productId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      });
+  
+      const data = await res.json();
+      if (data.success) {
+        toast.success(data.message);
+        setProducts(products.filter((p) => p._id !== productId));
+      } else {
+        toast.error(data.message);
+      }
+
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div className="max-w-2xl mx-auto">
       <div className="bg-white shadow-md rounded-lg max-w-sm">
-        <a href="#">
-          <Image
-            style={imageStyles}
-            src={product.image}
-            width={300}
-            height={400}
-            alt="product image"
-          />
-        </a>
+        <Image
+          style={imageStyles}
+          src={product.image}
+          width={300}
+          height={400}
+          alt="product image"
+        />
         <div className="flex flex-col gap-2 px-5 pb-5">
-          <a href="#">
-            <h3 className="text-gray-900 font-semibold text-xl tracking-tight">
-              Apple Watch {product.name}
-            </h3>
-          </a>
+          <h3 className="text-gray-900 font-semibold text-xl tracking-tight">
+            {product.name}
+          </h3>
           <div className="flex items-center justify-between">
             <span className="text-2xl font-bold text-gray-900">
               &#x20B9; {product.originalPrice}
@@ -40,14 +68,16 @@ const ProductCard = ({ product }) => {
           {isAdminView ? (
             <div className="flex items-center space-x-2 my-2">
               <button
+                onClick={handleUpdateClick}
                 type="button"
-                className="rounded md:w-36 bg-yellow-500 px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                className="rounded w-24 md:w-36 bg-yellow-500 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
               >
                 Update
               </button>
               <button
+               onClick={() => deleteAProduct(product._id)}
                 type="button"
-                className="rounded md:w-36 bg-red-500 px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                className="rounded w-24 md:w-36 bg-red-500 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
               >
                 Delete
               </button>
@@ -56,13 +86,13 @@ const ProductCard = ({ product }) => {
             <div className="flex items-center space-x-2 my-2">
               <button
                 type="button"
-                className="rounded md:w-36 bg-indigo-500 px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                className="rounded w-24 md:w-36 bg-indigo-500 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
               >
                 Details
               </button>
               <button
                 type="button"
-                className="rounded md:w-36 bg-green-500 px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                className="rounded px-3 md:w-36 bg-green-500 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
               >
                 Add To Cart
               </button>
