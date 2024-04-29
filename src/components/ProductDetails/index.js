6d1ad05727225ea7616d73ed32efb0ed797656e0
sanket560 <sanketmmane@gmail.com>
2024-04-29 20:37:06 +0530

    added ratings

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -2,19 +2,37 @@ import Image from "next/image";
 import React from "react";
 import { CiHeart } from "react-icons/ci";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { IoMdShare } from "react-icons/io";
+import { FaStar } from "react-icons/fa";
+import { FaStarHalfAlt } from "react-icons/fa";
+
 
 const ProductDetails = ({ product }) => {
-
   const formatPrice = (price) => {
     return price.toLocaleString("en-IN", {
       style: "currency",
       currency: "INR",
+      minimumFractionDigits: 0,
     });
   };
 
   return (
-    <section className="p-8 bg-white md:mt-10 md:py-16 rounded-md">
-      <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+    <section className="md:p-8 p-3 bg-white mt-10 md:py-16 rounded-md">
+      <div className="max-w-7xl px-4 mx-auto 2xl:px-0 relative">
+        <div className="absolute top-0 right-[16rem] md:right-[-75px] z-10">
+          <button
+            type="button"
+            className="w-10 md:w-max flex justify-center items-center mt-3 h-10 md:py-2 md:px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray1800 hover:bg-gray-100"
+          >
+            <CiHeart className="text-2xl" />
+          </button>
+          <button
+            type="button"
+            className="w-10 md:w-max flex justify-center items-center mt-3 h-10 md:py-2 md:px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100"
+          >
+            <IoMdShare className="text-2xl" />
+          </button>
+        </div>
         <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
           <div className="shrink-0 max-w-md lg:max-w-lg mx-auto">
             <Image
@@ -28,7 +46,14 @@ const ProductDetails = ({ product }) => {
             <h1 className="text-2xl font-semibold text-gray-900 sm:text-2xl">
               {product.name}
             </h1>
-            <div className="mt-4 flex items-center gap-2">
+            <div className="flex items-center gap-2 mt-3">
+              <span className="text-yellow-400"><FaStar/></span>
+              <span className="text-yellow-400"><FaStar/></span>
+              <span className="text-yellow-400"><FaStar/></span>
+              <span className="text-yellow-400"><FaStar/></span>
+              <span className="text-yellow-400"><FaStarHalfAlt/></span>
+            </div>
+            <div className="mt-3 flex items-center gap-2">
               <p className="text-2xl font-extrabold text-gray-900">
                 {formatPrice(product.discountPrice)}
               </p>
@@ -39,24 +64,16 @@ const ProductDetails = ({ product }) => {
                 </span>
               </p>
             </div>
-            <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
-              <a
-                href="#"
-                title
-                className="flex items-center justify-center py-2 px-5 text-sm gap-2 font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100"
-                role="button"
-              >
-                <CiHeart className="text-lg" />
-              </a>
+            <div className="mt-3 sm:gap-4 sm:items-center sm:flex">
               <button
                 type="button"
-                className="rounded flex items-center justify-center gap-2 w-52 bg-yellow-400 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                className="rounded flex w-full items-center justify-center gap-2 md:w-52 bg-yellow-400 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
               >
                 Buy Now
               </button>
               <button
                 type="button"
-                className="rounded flex items-center justify-center gap-2 w-52 bg-indigo-500 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                className="rounded flex w-full mt-2 md:mt-0 items-center justify-center gap-2 md:w-52 bg-indigo-500 md:px-6 py-1.5 font-medium leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
               >
                 <MdOutlineShoppingCart className="text-lg" /> Add To Cart
               </button>
