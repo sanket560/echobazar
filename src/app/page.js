9dadf04f78ce1e91a239b8698c4a78783df57ebc
sanket560 <sanketmmane@gmail.com>
2024-05-16 18:10:43 +0530

    updated carousel images

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
 import FeatureCards from "@/components/FeatureCards";
 import ImageSlideShow from "./../components/ImageSlideShow/index";
-import img1 from "../Images/img1.jpg";
 import img2 from "../Images/img2.jpg";
 import img3 from "../Images/img3.jpg";
 import LatestProduct from "@/components/LatestProduct";
 
 export default function Home() {
-  const images = [img1, img2, img3];
+  const images = [img2, img3];
   return (
     <div className="bg-gray-100 container mt-[43px] md:mt-0 mx-auto">
       <ImageSlideShow images={images} />
