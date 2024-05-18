ad9994dc80686fdba5fd40bad5b0df1622c42e81
sanket560 <sanketmmane@gmail.com>
2024-05-19 01:47:53 +0530

    fixed navbar issues

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,12 +5,14 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import logo from "../../Images/logo.svg";
 import { FiUser } from "react-icons/fi";
 import { RiMenu3Fill } from "react-icons/ri";
-import { IoIosArrowDown, IoMdClose } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { GlobalContext } from "@/context";
 import Cookies from "js-cookie";
 import { usePathname, useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { deleteFromCart, getAllCartItems } from "@/controller/cart";
+import { MdDelete } from "react-icons/md";
 
 const menuItemsOfDesktop = [
   {
@@ -77,8 +79,16 @@ const Navbar = () => {
   const pathname = usePathname();
   const router = useRouter();
 
-  const { isLoggedIn, setIsLoggedIn, userInfo, setUserInfo } =
-    useContext(GlobalContext);
+  const {
+    isLoggedIn,
+    setIsLoggedIn,
+    userInfo,
+    setUserInfo,
+    extractGetAllCartItems,
+    userCartData,
+    setUserCartData,
+    totalItemsInCart,
+  } = useContext(GlobalContext);
 
   const navbarRef = useRef(null);
   const isAdminView = pathname.includes("admin");
@@ -124,13 +134,38 @@ const Navbar = () => {
   const handleLogout = () => {
     setIsLoggedIn(false);
     setUserInfo(false);
+    setUserCartData([]);
     Cookies.remove("token");
     localStorage.clear();
     router.push("/");
     toast.success("Logout! See You Soon");
   };
 
-  // console.log(userInfo);
+  const formatPrice = (price) => {
+    return price.toLocaleString("en-IN", {
+      style: "currency",
+      currency: "INR",
+      minimumFractionDigits: 0,
+    });
+  };
+
+  const totalPrice = userCartData?.reduce((total, cartItem) => {
+    const itemTotalPrice = cartItem.discountPrice * cartItem.quantity;
+    cartItem.totalPrice = itemTotalPrice;
+    return total + itemTotalPrice;
+  }, 0);
+
+  async function deleteCartProduct(getId) {
+    const res = await deleteFromCart(getId);
+    extractGetAllCartItems();
+    if (res.success) {
+      toast.success("Product Removed From Cart");
+    } else {
+      toast.error("Failed to remove product");
+    }
+  }
+
+  // console.log(userCartData)
 
   return (
     <div className="shadow-md w-full z-30 fixed top-0 left-0" ref={navbarRef}>
@@ -184,59 +219,93 @@ const Navbar = () => {
         )}
         <div className="flex items-center">
           {!isAdminView && (
-            <div className="relative hidden md:block mr-2">
+            <div className="relative hidden md:block mr-4">
               <MdOutlineShoppingCart
                 onClick={() => setCartDropDown(!cartDropDown)}
                 className="w-7 mx-4 text-gray-800 h-7 cursor-pointer"
               />
               <span className="absolute top-0 right-2 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-                1
+                {totalItemsInCart}
               </span>
             </div>
           )}
 
           {cartDropDown && (
             <ul
-              className={`absolute z-10 w-min right-0 bg-white top-16 shadow-md py-2 rounded-b-md transition-all duration-300 ${cartDropDown ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}
+              className={`absolute hidden md:block z-10 w-min right-0 bg-white top-16 shadow-md py-2 rounded-b-md transition-all duration-300 ${cartDropDown ? "opacity-100" : "max-h-0 opacity-0"}`}
             >
-              <div className="relative w-screen max-w-sm px-4 py-8 sm:px-6 lg:px-8">
-                <div className="mt-4 space-y-6">
-                  <ul className="space-y-4">
-                    <li className="flex items-center gap-4">
-                      <img
-                        src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-                        alt=""
-                        className="size-16 rounded object-cover"
-                      />
+              <div className="relative w-screen max-w-sm px-4 pb-4 sm:px-6 lg:px-8">
+                <div className="mt-2 space-y-6">
+                  {(userCartData?.length ?? 0) > 0 ? (
+                    <>
+                      <ul>
+                        {userCartData.map((cartItem, index) => (
+                          <li
+                            key={index}
+                            className="flex py-3 border-b-2 md:w-[300px] items-center gap-4"
+                          >
+                            <Image
+                              src={cartItem.productID.image}
+                              alt={cartItem.productID.name}
+                              width={300}
+                              height={200}
+                              className="size-16 rounded object-cover"
+                            />
+                            <div className="w-full">
+                              <h3 className="text-sm text-gray-900">
+                                {cartItem.productID.name}
+                              </h3>
+                              <div className="mt-0.5 w-full flex justify-between space-y-px text-gray-600">
+                                <div className="w-full">
+                                  <div className="flex text-sm items-center gap-2">
+                                    <span>Price:</span>
+                                    <span>
+                                      {formatPrice(cartItem.discountPrice)}
+                                    </span>
+                                  </div>
+                                  <div className="flex items-center gap-2">
+                                    <div className="flex text-sm items-center gap-2">
+                                      <span>Quantity:</span>
+                                      <span>{cartItem.quantity}</span>
+                                    </div>
+                                    {cartItem.quantity > 1 && (
+                                      <div className="flex text-sm items-center gap-2">
+                                        <span>Total:</span>
+                                        <span>
+                                          {formatPrice(cartItem.totalPrice)}
+                                        </span>
+                                      </div>
+                                    )}
+                                  </div>
+                                </div>
+                                <button
+                                  onClick={() =>
+                                    deleteCartProduct(cartItem._id)
+                                  }
+                                >
+                                  <MdDelete className="text-red-400 text-xl" />
+                                </button>
+                              </div>
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
+                      <p className="text-sm text-gray-700">
+                        Overall Total: {formatPrice(totalPrice)}
+                      </p>
 
-                      <div>
-                        <h3 className="text-sm text-gray-900">
-                          Basic Tee 6-Pack
-                        </h3>
-
-                        <dl className="mt-0.5 space-y-px text-[10px] text-gray-600">
-                          <div>
-                            <dt className="inline">Size:</dt>
-                            <dd className="inline">XXS</dd>
-                          </div>
-
-                          <div>
-                            <dt className="inline">Color:</dt>
-                            <dd className="inline">White</dd>
-                          </div>
-                        </dl>
+                      <div className="space-y-4 text-center">
+                        <button
+                          onClick={() => router.push("/checkout")}
+                          className="block rounded bg-indigo-500 w-full px-5 py-3 text-sm text-white transition hover:bg-indigo-400"
+                        >
+                          Checkout
+                        </button>
                       </div>
-                    </li>
-                  </ul>
-
-                  <div className="space-y-4 text-center">
-                    <a
-                      href="#"
-                      className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
-                    >
-                      Checkout
-                    </a>
-                  </div>
+                    </>
+                  ) : (
+                    <p>Your cart is empty</p>
+                  )}
                 </div>
               </div>
             </ul>
@@ -300,9 +369,12 @@ const Navbar = () => {
         </div>
         <div className="md:hidden flex items-center">
           <div className="relative mr-2">
-            <MdOutlineShoppingCart className="w-7 mx-4 text-gray-800 h-7 cursor-pointer" />
+            <MdOutlineShoppingCart
+              onClick={() => setCartDropDown(!cartDropDown)}
+              className="w-7 mx-4 text-gray-800 h-7 cursor-pointer"
+            />
             <span className="absolute top-0 right-2 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-              1
+              {totalItemsInCart}
             </span>
           </div>
           <RiMenu3Fill
@@ -310,6 +382,84 @@ const Navbar = () => {
             className="h-6 w-6 cursor-pointer"
           />
         </div>
+        {cartDropDown && (
+          <ul
+            className={`absolute md:hidden z-10 w-full right-0 bg-white top-16 shadow-md pb-2 rounded-b-md transition-all duration-300 ${cartDropDown ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}
+          >
+            <div className="relative w-screen max-w-sm px-4 pb-2 sm:px-6 lg:px-8">
+              <div className="mt-4 space-y-6">
+                {(userCartData?.length ?? 0) > 0 ? (
+                  <>
+                    <ul>
+                      {userCartData.map((cartItem, index) => (
+                        <li
+                          key={index}
+                          className="flex py-3 border-b-2 w-full md:w-[300px] items-center gap-4"
+                        >
+                          <Image
+                            src={cartItem.productID.image}
+                            alt={cartItem.productID.name}
+                            width={300}
+                            height={200}
+                            className="size-16 rounded object-cover"
+                          />
+                          <div className="w-full">
+                            <h3 className="text-sm text-gray-900">
+                              {cartItem.productID.name}
+                            </h3>
+                            <div className="mt-0.5 w-full flex justify-between space-y-px text-gray-600">
+                              <div className="w-full">
+                                <div className="flex text-sm items-center gap-2">
+                                  <span>Price:</span>
+                                  <span>
+                                    {formatPrice(cartItem.discountPrice)}
+                                  </span>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                  <div className="flex text-sm items-center gap-2">
+                                    <span>Quantity:</span>
+                                    <span>{cartItem.quantity}</span>
+                                  </div>
+                                  {cartItem.quantity > 1 && (
+                                    <div className="flex text-sm items-center gap-2">
+                                      <span>Total:</span>
+                                      <span>
+                                        {formatPrice(cartItem.totalPrice)}
+                                      </span>
+                                    </div>
+                                  )}
+                                </div>
+                              </div>
+                              <button
+                                onClick={() => deleteCartProduct(cartItem._id)}
+                              >
+                                <MdDelete className="text-red-400 text-xl" />
+                              </button>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                    <p className="text-sm text-gray-700">
+                      Overall Total: {formatPrice(totalPrice)}
+                    </p>
+
+                    <div className="space-y-4 text-center">
+                      <button
+                        onClick={() => router.push("/checkout")}
+                        className="block rounded bg-indigo-500 w-full px-5 py-3 text-sm text-white transition hover:bg-indigo-400"
+                      >
+                        Checkout
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <p>Your cart is empty</p>
+                )}
+              </div>
+            </div>
+          </ul>
+        )}
         {isMenuOpen && (
           <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform transition lg:hidden">
             <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
