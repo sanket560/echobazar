b8ef23699a9b16cf031cfd2bb0bcdae7806dca5b
sanket560 <sanketmmane@gmail.com>
2024-05-19 01:46:23 +0530

    built address model

---
+++ src/models/Address.Model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const NewAddressSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Address =
+  mongoose.models.Address || mongoose.model("Address", NewAddressSchema);
+
+export default Address;
