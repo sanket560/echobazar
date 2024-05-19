bdc1f8ac4e6fd00bc1516ab108e6c6a30de90e5a
sanket560 <sanketmmane@gmail.com>
2024-05-19 19:33:59 +0530

    stripe controller

---
+++ src/controller/stripe.js
@@ -0,0 +1,18 @@
+import Cookies from "js-cookie";
+
+export const callStripeSession = async (formData) => {
+  try {
+    const res = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
