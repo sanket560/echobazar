3da32e5063c81714def810401965c01630e745e9
sanket560 <sanketmmane@gmail.com>
2024-05-25 00:16:37 +0530

    improved skelton for card

--- src/components/LatestProduct/index.js
+++ src/components/LatestProduct/index.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { GlobalContext } from "@/context";
 import React, { useContext } from "react";
 import ProductCard from "../ProductCard";
@@ -7,18 +7,24 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 const LatestProduct = () => {
   const { allProduct } = useContext(GlobalContext);
-  const latestProducts = allProduct.filter(product => product.latestProduct === "yes");
+  const latestProducts = allProduct.filter(
+    (product) => product.latestProduct === "yes"
+  );
 
   return (
     <div className="md:w-[1450px] px-5 pb-10 md:pb-20 mx-auto">
       <p className="font-semibold mb-4 text-3xl">Latest Products</p>
       {latestProducts.length == 0 ? (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
-          {[...Array(4)].map((_, index) => (
-            <div key={index}>
-              <Skeleton width={280} height={350}/>
-            </div>
-          ))}
+          <div className="hidden md:flex md:space-x-4 lg:space-x-8">
+            <Skeleton width={330} height={350} />
+            <Skeleton width={330} height={350} />
+            <Skeleton width={330} height={350} />
+            <Skeleton width={330} height={350} />
+          </div>
+          <div className="block md:hidden">
+            <Skeleton width={349} height={350} count={3} />
+          </div>
         </div>
       ) : (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
