e83f2c4902c94f79b4b6692188570b9e424c02ef
sanket560 <sanketmmane@gmail.com>
2024-05-27 11:33:35 +0530

    fixed image remote urls

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com","firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '**',
+      },
+    ],
   },
 };
 
