4d5ca89ee8c07370664e01b2917793dd16d6b4e9
sanket560 <sanketmmane@gmail.com>
2024-05-30 10:12:41 +0530

    added cool UI for uploading image

--- src/components/PhoneForm/index.js
+++ src/components/PhoneForm/index.js
@@ -1,46 +1,7 @@
 "use client";
 import React, { useContext, useEffect, useState } from "react";
-import {
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytesResumable,
-} from "firebase/storage";
-import { initializeApp } from "firebase/app";
-import { firebaseConfig, firebaseStorageURL } from "@/utils/firebaseConfig";
 import { GlobalContext } from "@/context";
 import Image from "next/image";
-
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app, firebaseStorageURL);
-
-const createUniqueFileName = (getFile) => {
-  const timeStamp = Date.now();
-  const randomStringValue = Math.random().toString(36).substring(2, 12);
-  return `${getFile.name}-${timeStamp}-${randomStringValue}`;
-};
-
-const helperForUploadingImageToFirebase = async (file) => {
-  const getFileName = createUniqueFileName(file);
-  const storageReference = ref(storage, `ecobazar/${getFileName}`);
-  const uploadImage = uploadBytesResumable(storageReference, file);
-
-  return new Promise((resolve, reject) => {
-    uploadImage.on(
-      "state_changed",
-      (snapshot) => {},
-      (error) => {
-        console.log(error);
-        reject(error);
-      },
-      () => {
-        getDownloadURL(uploadImage.snapshot.ref)
-          .then((downloadUrl) => resolve(downloadUrl))
-          .catch((error) => reject(error));
-      }
-    );
-  });
-};
 
 // Phone Form Component
 export const PhoneForm = ({ onSubmit, isLoading, selectedProductToUpdate }) => {
@@ -68,7 +29,7 @@ export const PhoneForm = ({ onSubmit, isLoading, selectedProductToUpdate }) => {
   const [discountPrice, setDiscountPrice] = useState(
     selectedProductToUpdate ? selectedProductToUpdate.discountPrice : ""
   );
-  const [latestProduct, setlatestProduct] = useState(
+  const [latestProduct, setLatestProduct] = useState(
     selectedProductToUpdate ? selectedProductToUpdate.latestProduct : "yes"
   );
   const [selectedImage, setSelectedImage] = useState(
@@ -77,73 +38,27 @@ export const PhoneForm = ({ onSubmit, isLoading, selectedProductToUpdate }) => {
   const { userInfo } = useContext(GlobalContext);
   const sellerName = userInfo?.name;
 
-  const handleImageChange = async (e) => {
-    const extractImageUrl = await helperForUploadingImageToFirebase(
-      e.target.files[0]
-    );
-    // console.log(extractImageUrl);
-    if (extractImageUrl !== "") {
-      setImage(extractImageUrl);
-      setSelectedImage(extractImageUrl);
-    }
-  };
+  const CLOUD_NAME = "df0tgq0yy";
+  const UPLOAD_PRESET = "NEXTJS_echobazar";
 
-  const handleRemoveImage = () => {
-    setImage(""); // Clear the image state
-    setSelectedImage(null); // Clear the selected image state
-  };
+  const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append("file", selectedImage);
+    formData.append("upload_preset", UPLOAD_PRESET);
 
-  const renderImageUpload = () => {
-    if (selectedImage) {
-      return (
-        <div className="mb-4">
-          <Image src={selectedImage} width={200} height={300} alt="Selected" className="w-96 mx-auto" />
-          <button type="button" onClick={handleRemoveImage} className="mt-2 text-red-500">
-            Remove Image
-          </button>
-        </div>
+    try {
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
       );
-    } else {
-      return (
-        <div className="mb-4">
-          <label
-            htmlFor="image"
-            className="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-blue-400 bg-white p-6 text-center"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-10 w-10 text-blue-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              />
-            </svg>
 
-            <h2 className="mt-4 text-xl font-medium text-gray-700 tracking-wide">
-              Upload Product Image
-            </h2>
-
-            <p className="mt-2 text-gray-500 tracking-wide">
-              Upload or drag & drop your image (PNG, JPG).
-            </p>
-
-            <input
-              id="image"
-              type="file"
-              onChange={handleImageChange}
-              accept="image/*"
-              className="hidden"
-              required
-            />
-          </label>
-        </div>
-      );
+      const data = await res.json();
+      return data["secure_url"];
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -179,12 +94,15 @@ export const PhoneForm = ({ onSubmit, isLoading, selectedProductToUpdate }) => {
     setStorageOptions(newStorageOptions);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const imageUrl = await uploadImage();
+
     const formData = {
       name,
       brand,
-      image,
+      image: imageUrl,
       colors,
       storageOptions,
       description,
@@ -196,19 +114,50 @@ export const PhoneForm = ({ onSubmit, isLoading, selectedProductToUpdate }) => {
     onSubmit(formData);
   };
 
+  const handleImageRemove = () => {
+    setSelectedImage(null);
+  };
+
   const isValidForm = () => {
-    return name &&
+    return (
+      name &&
       name.trim() !== "" &&
       typeof discountPrice === "string" &&
       discountPrice.trim() !== ""
-      ? true
-      : false;
+    );
   };
 
   return (
     <div className="max-w-md mx-auto p-4 bg-white mt-8">
       <form onSubmit={handleSubmit}>
-        {renderImageUpload()}
+      <h2 className="my-4 text-xl font-medium text-gray-700 tracking-wide">
+          Upload Product Image
+        </h2>
+        {selectedImage ? (
+          <div className="mb-4">
+            <img src={URL.createObjectURL(selectedImage)} alt="Selected" className="mb-2" />
+            <button
+              type="button"
+              onClick={handleImageRemove}
+              className="bg-red-500 text-white py-1 px-3 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300"
+            >
+              Remove Image
+            </button>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center w-full mb-4">
+            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                </svg>
+                <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                <p className="text-xs text-gray-500">Upload only PNG or JPG</p>
+              </div>
+              <input id="dropzone-file" type="file" className="hidden" onChange={(e) => setSelectedImage(e.target.files[0])} />
+            </label>
+          </div>
+        )}
         <div className="mb-4">
           <label
             htmlFor="name"
@@ -392,7 +341,7 @@ export const PhoneForm = ({ onSubmit, isLoading, selectedProductToUpdate }) => {
           <select
             id="latestProduct"
             value={latestProduct}
-            onChange={(e) => setlatestProduct(e.target.value)}
+            onChange={(e) => setLatestProduct(e.target.value)}
             className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-indigo-300"
             required
           >
