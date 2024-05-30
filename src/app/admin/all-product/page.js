fd4c2f05b77921770b663f6ceddd156dc185d8a2
sanket560 <sanketmmane@gmail.com>
2024-05-30 10:11:14 +0530

    added skeleton for products

--- src/app/admin/all-product/page.js
+++ src/app/admin/all-product/page.js
@@ -3,40 +3,22 @@ import { GlobalContext } from "@/context";
 import { useRouter } from "next/navigation";
 import ProductCard from "@/components/ProductCard";
 import React, { useContext, useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 
 const FRONTEND_BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_BASE_URL;
 
 const AllProduct = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
   const { isLoggedIn, userInfo } = useContext(GlobalContext);
 
   useEffect(() => {
-    if (!isLoggedIn && userInfo?.role !== "Seller") {
-      router.push('/');
+    if (!isLoggedIn || userInfo?.role !== "Seller") {
+      router.push("/");
     }
   }, [isLoggedIn, userInfo?.role, router]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `${FRONTEND_BASE_URL}/api/admin/all-product`,
-          {
-            method: "GET",
-          }
-        );
-        const responseData = await response.json();
-        setProducts(responseData?.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []); // Fetch data only once when component mounts
-
-  // Function to fetch data when products state changes
   const fetchProducts = async () => {
     try {
       const response = await fetch(
@@ -46,17 +28,35 @@ const AllProduct = () => {
         }
       );
       const responseData = await response.json();
-      setProducts(responseData?.data);
+      setProducts(responseData?.data || []);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
+      setLoading(false);
     }
   };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <div className="flex items-center justify-center mt-10 px-5">
       <div className="py-6 sm:py-4 lg:py-12">
         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-          {products.length === 0 ? (
+          {loading ? (
+            <>
+              <div className="hidden md:flex md:space-x-4 lg:space-x-8">
+                <Skeleton width={330} height={350} />
+                <Skeleton width={330} height={350} />
+                <Skeleton width={330} height={350} />
+                <Skeleton width={330} height={350} />
+              </div>
+              <div className="block md:hidden">
+                <Skeleton width={349} height={350} count={3} />
+              </div>
+            </>
+          ) : products.length === 0 ? (
             <p className="text-center text-gray-500">
               You don&apos;t have any products.
             </p>
@@ -66,7 +66,7 @@ const AllProduct = () => {
                 <ProductCard
                   key={product._id}
                   product={product}
-                  fetchProducts={fetchProducts}
+                  fetchProducts={fetchProducts} 
                 />
               ))}
             </div>
