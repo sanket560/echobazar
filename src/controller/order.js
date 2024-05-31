a8401080b8636b708023d4c698d67f8073552c5e
sanket560 <sanketmmane@gmail.com>
2024-05-31 23:41:45 +0530

    created controller for getAllOrderForAllUser and updateStatusOfOrder

--- src/controller/order.js
+++ src/controller/order.js
@@ -49,3 +49,37 @@ export const getOrderDetails = async (id) => {
     console.log(e);
   }
 };
+
+export const getAllOrderForAllUser = async () => {
+  try {
+    const res = await fetch(`/api/admin/orders/get-all-orders`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const updateStatusOfOrder = async (formData) => {
+  try {
+    const res = await fetch(`/api/admin/orders/update-order`, {
+      method: "PUT",
+      headers: {
+        "Content-type" : 'application/json',
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body : JSON.stringify(formData)
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
