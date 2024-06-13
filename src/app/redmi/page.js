33af83ea4829280235f65deea75e80b09fc4e288
sanket560 <sanketmmane@gmail.com>
2024-06-13 13:01:38 +0530

    fixed deployment page name issue

--- src/app/redmi/page.js
+++ src/app/redmi/page.js
@@ -5,7 +5,7 @@ import React, { useContext } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-const page = () => {
+const Page = () => {
   const { allProduct } = useContext(GlobalContext);
   const latestProducts = allProduct.filter(
     (product) => product.brand === "Redmi"
@@ -13,7 +13,7 @@ const page = () => {
 
   return (
     <div className="md:w-[1450px] px-5 pb-10 mt-10 md:mt-20 md:pb-20 mx-auto">
-      {latestProducts.length == 0 ? (
+      {latestProducts.length === 0 ? (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
           <div className="hidden md:flex md:space-x-4 lg:space-x-8">
             <Skeleton width={330} height={350} />
@@ -36,4 +36,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
